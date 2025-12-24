c859acf880acdfb5a09bb62646f960c1073cf0aa
ph4mished <jbotchwayowusu@gmail.com>
2025-12-24 13:46:30 +0000

    Fixed RGB IndexDefect Bug

--- src/spectra/chain.nim
+++
@@ -1,5 +0,0 @@
-import color
-
-#let inner = parse("[bold][fg=red]Danger[reset]")
-let outer = parse("[bold fg=red]Warning: [s][reset]")
-echo outer.apply() #inner.apply())
--- src/spectra/test.nim
+++
@@ -1,6 +0,0 @@
-import color
-
-#let hi =  parse("[bold fg=red] [s][fg=#ffbbaa] Hello [fg=34]World [fg=rgb(25,24,209)]Welcome[reset]")
-#echo hi.apply()
-listColors()
-listStyles()
